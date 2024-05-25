50b59d7699f8d148b786e8c5c9b6b7a32ba2e397
Sanjay Mali <sanjaymali94284@gmail.com>
2024-05-25 17:00:01 +0530

    feat: Update RichTextEditor component with TinyMCE API key

--- src/components/RichTextEditor.jsx
+++ src/components/RichTextEditor.jsx
@@ -1,18 +1,22 @@
 import React from "react";
-import { Editor } from "@tinymce/tinymce-react";
 import { Controller } from "react-hook-form";
+import { Editor } from "@tinymce/tinymce-react";
+import config from "../config/config";
 
 function RichTextEditor({ name, control, label, defaultValue = "" }) {
   return (
     <div className="w-full">
       {label && <label className="inline-block mb-1 pl-1">{label}</label>}
+
       <Controller
         name={name || "content"}
         control={control}
-        render={({ field: { onChange } }) => {
+        render={({ field: { onChange } }) => (
           <Editor
             initialValue={defaultValue}
+            apiKey={config.tinyMCEKey}
             init={{
+              initialValue: defaultValue,
               height: 500,
               menubar: true,
               plugins: [
@@ -43,8 +47,8 @@ function RichTextEditor({ name, control, label, defaultValue = "" }) {
                 "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
             }}
             onEditorChange={onChange}
-          />;
-        }}
+          />
+        )}
       />
     </div>
   );
