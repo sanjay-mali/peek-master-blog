a5f19d601d56232cee3e48c2950ee465560696be
Sanjay Mali <sanjaymali94284@gmail.com>
2024-05-25 16:58:37 +0530

    feat: Add react-router-dom for managing app navigation
    This commit adds the react-router-dom library to the project to enable client-side routing and navigation. It also updates the main.jsx file to include the necessary components and routes for the different pages of the app. This will improve the user experience by allowing them to navigate between different pages without a full page reload.

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,82 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { Provider } from 'react-redux'
-import store from './store/store.js'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Provider } from "react-redux";
+import store from "./store/store.js";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { AuthLayout } from "./components";
+import {
+  HomePage,
+  LoginPage,
+  SignupPage,
+  AllPost,
+  AddPost,
+  EditPost,
+  Post,
+} from "./pages/index.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/login",
+        element: (
+          <AuthLayout authentication={false}>
+            <LoginPage />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/signup",
+        element: (
+          <AuthLayout authentication={false}>
+            <SignupPage />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/all-post",
+        element: (
+          <AuthLayout authentication>
+            <AllPost />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/edit-post/:slug",
+        element: (
+          <AuthLayout authentication>
+            <EditPost />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/post/:slug",
+        element: <Post />,
+      },
+      {
+        path: "/create-post",
+        element: (
+          <AuthLayout authentication>
+            <AddPost />
+          </AuthLayout>
+        ),
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <RouterProvider router={routes} />
     </Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
