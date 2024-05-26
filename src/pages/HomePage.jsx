3b6d2ac7ae8506ec67c00fae00296c9db45aaf21
Sanjay Mali <sanjaymali94284@gmail.com>
2024-05-26 21:21:45 +0530

    feat: Fix conditional check for posts length in HomePage.jsx
    The commit fixes a bug in the HomePage.jsx file where the conditional check for the length of the "posts" array was not properly handling the case when "posts" is null. The fix adds an additional check to ensure that "posts" is not null before checking its length. This ensures that the correct behavior is maintained when rendering the component.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -32,14 +32,15 @@ function HomePage() {
     <div className="w-full">
       <Container>
         <div className="flex flex-wrap -m-2">
-          {posts.map((post) => (
-            <div
-              key={post.$id}
-              className="p-2 w-full sm:w-1/2 md:w-1/3 lg:w-1/4"
-            >
-              <PostCard {...post} />
-            </div>
-          ))}
+          {posts &&
+            posts.map((post) => (
+              <div
+                key={post.$id}
+                className="p-2 w-full sm:w-1/2 md:w-1/3 lg:w-1/4"
+              >
+                <PostCard {...post} />
+              </div>
+            ))}
         </div>
       </Container>
     </div>
