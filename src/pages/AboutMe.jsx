7ad8ff8409a35c9299a30e0515fa6aa9ef648be3
Sanjay Mali <sanjaymali94284@gmail.com>
2024-05-27 14:53:11 +0530

    feat: Update AboutMe page content and styles

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,29 +1,56 @@
 import React from "react";
 
-function AboutMe() {
+const AboutMe = () => {
   return (
-    <>
-        <div
-          className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center
-            bg-[url('https://images.unsplash.com/photo-1716569643976-215601ace5e4?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]
-            bg-center w-full h-full z-0 brightness-75 bg-no-repeat flex flex-col justify-center items-center
-            "
-        >
-          <h1 className="text-4xl md:text-6xl lg:text-4xl font-bold text-white">
-            About Me
-          </h1>
-          <p className="text-white text-lg md:text-2xl lg:text-3xl mt-4">
-            I am a full-stack developer
+    <div className="min-h-[85vh] flex items-center justify-center bg-cover bg-center">
+      <div className="bg-white bg-opacity-75 p-8 rounded-lg shadow-lg flex flex-col md:flex-row items-center md:items-start max-w-4xl mx-auto space-y-8 md:space-y-0 md:space-x-8">
+        <div className="flex-1 text-center md:text-left">
+          <h1 className="text-4xl font-bold mb-4">Sanjay Mali</h1>
+          <p className="mb-4">
+            I am an aspiring front-end developer with a strong foundation in
+            HTML, CSS, and JavaScript, complemented by hands-on experience with
+            frameworks like React. I am eager to apply my coding skills,
+            creativity, and passion for web development to contribute to
+            innovative projects and enhance user experiences. I am motivated to
+            grow and learn in a collaborative and dynamic environment.
           </p>
+          <div className="flex justify-center md:justify-start space-x-4">
+            <a
+              href="https://x.com/BoltHacker1"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-500 hover:text-blue-700 mt-4 border border-gray-300 p-3 rounded-md"
+            >
+              Twitter
+            </a>
+            <a
+              href="https://linkedin.com/in/sanjay-mali-dev"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-500 hover:text-blue-700 mt-4 border border-gray-300 p-3 rounded-md "
+            >
+              LinkedIn
+            </a>
+            <a
+              href="https://github.com/sanjay-mali"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-500 hover:text-blue-700 mt-4 border border-gray-300 p-3 rounded-md "
+            >
+              GitHub
+            </a>
+          </div>
         </div>
-        {/* <img
-            src="https://images.unsplash.com/photo-1716569643976-215601ace5e4?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="background Images"
-            className="w-full h-[100vh] brightness-75 object-cover"
-          /> */}
-     
-    </>
+        <div className="flex-1">
+          <img
+            src="/src/assets/3d-man.jpg"
+            alt="3D Representation"
+            className="w-full h-auto rounded-lg shadow-lg"
+          />
+        </div>
+      </div>
+    </div>
   );
-}
+};
 
 export default AboutMe;
